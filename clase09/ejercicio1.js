a8103f59324c571a0aa3738e234994f826864d8d
Tyncho <tyncho.zabala@gmail.com>
2025-10-29 09:03:26 -0300

    primer script JS

---
+++ clase09/ejercicio1.js
@@ -0,0 +1,12 @@
+let num1 = prompt("Ingrese el primer número: ");
+let num2 = prompt("Ingrese el segundo número");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+console.log("Estos son los resultados de las operaciones");
+console.log("Suma: "+(num1+num2));
+console.log("Resta: "+(num1-num2));
+console.log("Multiplicación: "+(num1*num2));
+console.log("División: "+(num1/num2));
+console.log("Módulo: "+(num1%num2));
