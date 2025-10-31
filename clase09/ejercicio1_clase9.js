3dd5219c9bb7c08beac57664b4dcab26088732d2
Tyncho <tyncho.zabala@gmail.com>
2025-10-30 21:09:46 -0300

    Ejercicio 1 Clase 9 le agregue el texto en html con getElementById

--- clase09/ejercicio1_clase9.js
+++ clase09/ejercicio1_clase9.js
@@ -9,6 +9,7 @@ num2 = parseFloat(num2);
 con el "!" delante de isNaN porque debe negar el resultado.
 isNaN(valor) devuelve true si el valor no es un número.
 Pero yo quiero saber si sí es un número válido, o sea lo contrario.*/
+
 console.log("¿El primer dato es un número válido?: " + !isNaN(num1));
 console.log("¿El segundo dato es un número válido?: " + !isNaN(num2));
 
@@ -19,3 +20,17 @@ console.log("Resta: "+(num1-num2));
 console.log("Multiplicación: "+(num1*num2));
 console.log("División: "+(num1/num2));
 console.log("Módulo: "+(num1%num2));
+
+//Con getElementById puedo escribir dentro del HTML, inclusive podria usar las etiquetas.
+document.getElementById("resultado").innerHTML =
+    "Los números ingresados son el <strong>" + num1 + "</strong> y el <strong>" + num2 + "</strong><br><br>" +
+    "¿El primer dato es un número válido?: " + !isNaN(num1) + "<br>" +
+    "¿El segundo dato es un número válido?: " + !isNaN(num2) + "<br><br>" +
+    "Estos son los resultados de las operaciones:<br>" +
+    "<ul>"+
+    "<li>Suma: " + (num1 + num2) + "</li>" +
+    "<li>Resta: " + (num1 - num2) + "</li>" +
+    "<li>Multiplicación: " + (num1 * num2) + "</li>" +
+    "<li>División: " + (num1 / num2) + "</li>" +
+    "<li>Módulo: " + (num1 % num2) + "</li>"+
+    "</ul>";
